fa227dfe0e963c44b27afde4db7df3185b9dd16c
unknown <zubair7069@gmail.com>
2023-08-20 14:50:03 +0500

    function

---
+++ function.ts
@@ -0,0 +1,18 @@
+// let studentName:string[]=["zubiar", "ALi", "irfan", "Naveed", "kamran"];
+// // console.log(studentName);
+// studentName.push("Abdullah");
+// studentName.pop();
+// studentName.shift();
+// studentName.unshift("Arslan");
+// studentName.splice(4,1, "Umair");
+// studentName.splice(3,1, "Arshad");
+// studentName.splice(4,0, "Waleed");
+// studentName.splice(4,1,);
+// studentName.splice(4,0);
+
+let todos:string[]=["Learn Array","Learn Array Method","Learn Condition", "learn If Case", " Practics Daily"];
+// if (todos.length%2==0){
+//     let todos1= todos.splice(1,1);
+//     console.log(todos1)
+// }
+console.log(todos);
